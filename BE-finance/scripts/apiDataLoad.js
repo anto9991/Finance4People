58bd87915055a8cd92ed6cdb8c885d9f2029d274
Antonel Gabor <antonelgabor@Antonels-MBP.station>
2022-10-21 10:56:33 +0200

    Added recap system with email notification

--- BE-finance/scripts/apiDataLoad.js
+++ BE-finance/scripts/apiDataLoad.js
@@ -1,3 +1,4 @@
+'use strict';
 console.log("---------- Start load data exexution ----------\n");
 // https://data.nasdaq.com/api/v3/datatables/MER/F1.xml?&mapcode=-3851&compnumber=39102&reporttype=A&qopts.columns=reportdate,amount&
 // api_key=<YOURAPIKEY>
@@ -14,6 +15,8 @@ const apikey = env.FMP_API_KEY;
 const today = new Date;
 const agent = "Load script";
 const index = "SP500"
+const utils = require('./utils')
+const fs = require('fs');
 
 // f stands for formatted
 const fToday = today.toISOString().split('T')[0]
@@ -41,6 +44,13 @@ function dbConnection() {
     } catch (err) {
         throw err;
     }
+}
+
+let recap = {
+    timestamp: new Date(),
+    filename: "dataLoad_" + fToday + ".json",
+    errors: [],
+    ok: []
 }
 
 
@@ -102,7 +112,7 @@ async function FMPmain() {
     }
 }
 async function YahooMain() {
-
+    // Create DB instance & get instance
     let dbInstance;
     try {
         dbInstance = await dbConnection()
@@ -111,94 +121,193 @@ async function YahooMain() {
         // Break and notify
     }
     let dbStocks = dbInstance.db(env.DB_NAME).collection("Stocks")
+    // Get csv's SP500 stocks
     let stockList = await getCSVStockList();
 
-    let stock = stockList[0];
+    for (let index = 451; index <= 452;index++) {
+        let stock = stockList[index];
+        try {
+            // Yahoo finance uses minus instead of dot
+            if (stock.Symbol.indexOf(".") >= 0) stock.Symbol = stock.Symbol.replace('.', '-')
 
-    let options = {
-        params: {
-            'symbols': 'AAPL'
-        }
-    }
-
-
-
-    let financeStatsApi = await axios.get("https://query1.finance.yahoo.com/v7/finance/quote", options)
-        .then((res) => { return res.data; })
-        .catch((err) => { console.log("Logging apiErr: \n", err); return "Error" })
-
-    let financeStats = financeStatsApi.quoteResponse.result[0]
-
-    let stockId;
-    let getDBStock = await dbStocks.findOne({ ticker: stock.Symbol })
-
-    // If not create stock record
-    if (getDBStock == null) {
-        let insertNewStock = await dbStocks.insertOne(
-            {
-                ticker: stock.Symbol,
-                name: stock.Description,
-                sector: stock["GICS Sector"],
-                created_at: today.getTime(),
-                created_by: agent,
-                index: index,
-                series: [],
-                currency: financeStats.currency,
-                keyStatistics: []
-            }
-        )
-        if (!insertNewStock.acknowledged) throw new Error("Stock insert failed");
-        else stockId = insertNewStock.insertedId;
-    } else {
-        stockId = getDBStock._id;
-    }
-
-    options = {
-        params: {
-            'modules': 'defaultKeyStatistics'
-        }
-    }
-    let keyStatsApi = await axios.get("https://query1.finance.yahoo.com/v11/finance/quoteSummary/AAPL", options)
-        .then((res) => { return res.data; })
-        .catch((err) => { console.log("Logging apiErr: \n", err); return "Error" })
-
-    let keyStats = keyStatsApi.quoteSummary.result[0].defaultKeyStatistics
-
-    let dataHash = md5(
-        financeStats.marketCap + " " +
-        financeStats.forwardPE + " " +
-        financeStats.trailingPE + " " +
-        financeStats.trailingEps + " " +
-        financeStats.epsTrailingTwelveMonths + " " +
-        financeStats.averageAnalystRating + " " +
-        financeStats.fiftyTwoWeekLow + " " +
-        financeStats.fiftyTwoWeekHigh)
-
-    let insertFinance = await dbStocks.updateOne(
-        { _id: stockId, $and: [{"keyStatistics.dataHash": { $ne: dataHash }}, {"keyStatistics.date": {$ne: fToday }}] },
-        {
-            $push: {
-                keyStatistics: {
-                    date: fToday,
-                    data: {
-                        // key stats
-                        beta: keyStats.beta.raw,
-                        // finance request
-                        marketCap: financeStats.marketCap,
-                        forwardPE: financeStats.forwardPE,
-                        trailingPE: financeStats.trailingPE,
-                        trailingEPS: financeStats.trailingEps,
-                        forwardEPS: financeStats.epsTrailingTwelveMonths,
-                        averageAnalystRating: financeStats.averageAnalystRating,
-                        fiftyTwoWeekLow: financeStats.fiftyTwoWeekLow,
-                        fiftyTwoWeekHigh: financeStats.fiftyTwoWeekHigh
-                    },
-                    dataHash: dataHash
+            let options;
+            // 1° API request: basic data like market cap etc...
+            options = {
+                params: {
+                    'symbols': stock.Symbol
                 }
             }
-        },
-    )
+            let financeStatsApi = await axios.get("https://query1.finance.yahoo.com/v7/finance/quote", options)
+                .then((res) => {
+                    return res.data;
+                })
+                .catch((err) => {
+                    recap.errors.push({
+                        section: "First API call",
+                        ticker: stock.Symbol,
+                        data: err
+                    })
+                    console.log("Logging apiErr: \n", err);
+                })
+            let financeStats = financeStatsApi.quoteResponse.result[0]
+
+            if(!financeStats){
+                recap.errors.push({
+                    section: "First API request",
+                    ticker: stock.Symbol,
+                    data: "Not found in Yahoo, stocklist.csv might need an update"
+                })
+                continue
+            }
+
+            // Line up the DB
+            // Check stock existence on DB
+            let stockId;
+            let getDBStock = await dbStocks.findOne({ ticker: stock.Symbol })
+            if (getDBStock == null) {
+                let insertNewStock = await dbStocks.insertOne(
+                    {
+                        ticker: stock.Symbol,
+                        name: stock.Description,
+                        sector: stock["GICS Sector"],
+                        created_at: today.getTime(),
+                        created_by: agent,
+                        index: index,
+                        series: [],
+                        currency: financeStats.currency,
+                        keyStatistics: []
+                    }
+                )
+                if (!insertNewStock.acknowledged || !insertNewStock.insertedId) {
+                    recap.errors.push({
+                        section: "Stock insert",
+                        ticker: stock.Symbol,
+                        data: insertNewStock
+                    })
+                    continue
+                }
+
+                stockId = insertNewStock.insertedId;
+            } else {
+                stockId = getDBStock._id;
+            }
+
+            // 2° Request: more refined data like beta etc...
+            options = {
+                params: {
+                    'modules': 'defaultKeyStatistics'
+                }
+            }
+            let keyStatsApi = await axios.get("https://query1.finance.yahoo.com/v11/finance/quoteSummary/" + stock.Symbol, options)
+                .then((res) => {
+                    return res.data;
+                })
+                .catch((err) => {
+                    recap.errors.push({
+                        section: "Second API call",
+                        ticker: stock.Symbol,
+                        data: err
+                    })
+                    console.log("Logging apiErr: \n", err);
+                    return "Error"
+                })
+            fs.writeFileSync("example2.json", JSON.stringify(keyStatsApi));
+
+            // No particular reason for index 0, just json structure
+            let keyStats = keyStatsApi.quoteSummary.result[0].defaultKeyStatistics
+
+            // 3° Request: specific financials data as PPE, EBIT etc...
+            options = {
+                params: {
+                    "p": stock.Symbol
+                }
+            }
+            let financialsApi = await axios.get("https://finance.yahoo.com/quote/" + stock.Symbol + "/balance-sheet", options)
+                .then((res) => {
+                    return res.data;
+                })
+                .catch((err) => {
+                    recap.errors.push({
+                        section: "Second API call",
+                        ticker: stock.Symbol,
+                        data: err
+                    })
+                    console.log("Logging apiErr: \n", err);
+                })
+
+            // This request doesn't return a simple json object but a full web page (js included with json variable containing all data)
+            let parsedData = JSON.parse(utils.subStringCustom(financialsApi, 'root.App.main', '(this));\n</script><script>', 16, -3));
+            let financials = parsedData.context.dispatcher.stores.QuoteSummaryStore
+
+            // El in pos 0 => last quarter(trimestre) prop (check yahoo finance to undestand why)
+            let currentLiabilities = financials.balanceSheetHistoryQuarterly.balanceSheetStatements[0].totalCurrentLiabilities
+            let currentAssets = financials.balanceSheetHistoryQuarterly.balanceSheetStatements[0].totalCurrentAssets
+            let netPPE = financials.balanceSheetHistoryQuarterly.balanceSheetStatements[0].propertyPlantEquipment
+            let ebit = financials.incomeStatementHistoryQuarterly.incomeStatementHistory[0].ebit
+
+            let obj = {
+                // key stats
+                beta: keyStats.beta,
+                enterpriseValue: keyStats.enterpriseValue,
+                // finance request
+                marketCap: financeStats.marketCap,
+                forwardPE: financeStats.forwardPE,
+                trailingPE: financeStats.trailingPE,
+                trailingEPS: financeStats.trailingEps,
+                forwardEPS: financeStats.epsTrailingTwelveMonths,// r u sure m8?
+                averageAnalystRating: financeStats.averageAnalystRating,
+                fiftyTwoWeekLow: financeStats.fiftyTwoWeekLow,
+                fiftyTwoWeekHigh: financeStats.fiftyTwoWeekHigh,
+                // financials section
+                currentLiabilities: currentLiabilities,
+                currentAssets: currentAssets,
+                ebit: ebit,
+                netPPE: netPPE
+            }
+
+            // Check date to update DB only once a day
+            let insertFinance = await dbStocks.updateOne(
+                { _id: stockId, $or: [{ "keyStatistics.date": { $ne: fToday } }, { "keyStatistics.date": { $exists: false } }] },
+                {
+                    $push: {
+                        keyStatistics: {
+                            date: fToday,
+                            data: obj
+                        }
+                    }
+                },
+            )
+
+            if (!insertFinance.acknowledged || !(insertFinance.modifiedCount > 0)) {
+                recap.errors.push({
+                    section: "Keystats insert",
+                    ticker: stock.Symbol,
+                    data: insertFinance
+                })
+                continue
+            }
+            recap.ok.push({
+                index: index,
+                ticker: stock.Symbol,
+            })
+
+        }catch(err){
+            console.log(err)
+            continue
+        }finally{
+            console.log("Sleep, stock " + stock.Symbol + " done")
+            await utils.delay(30000)
+            console.log("Waking up")
+        }
+    }
+
     dbInstance.close()
+
+    fs.writeFileSync("./log/" + recap.filename, JSON.stringify(recap))
+
+    // if (recap.errors.length > 0) {
+    //     utils.sendEmail("./log/" + recap.filename, recap.filename, env.GMAIL_PWD, "antonelgabor@gmail.com");
+    // }
 
 }
 
---
+++ BE-finance/scripts/utils.js
@@ -0,0 +1,67 @@
+const mailer = require('nodemailer');
+
+function findByKey(obj, keyToFind) {
+    return Object.entries(obj)
+        .reduce((acc, [key, value]) => {
+            if (key === keyToFind) {
+                return acc.concat(value)
+            } else {
+                if (typeof value === 'object') {
+                    return acc.concat(findByKey(value, keyToFind))
+                } else {
+                    return acc
+                }
+            }
+        }, [])
+}
+
+// function findByKey(obj, keyToFind) {
+//     return Object.entries(obj)
+//         .reduce((acc, [key, value]) => (key === keyToFind)
+//             ? acc.concat(value)
+//             : (typeof value === 'object') 
+//                 ? acc.concat(findByKey(value, keyToFind))
+//                 : acc
+//             , [])
+// }
+
+function subStringCustom(input, start, end, startOffset, endOffset) {
+    let startIndex = input.indexOf(start) + startOffset;
+    let endIndex = input.indexOf(end) + endOffset;
+    return input.substr(startIndex, endIndex - startIndex)
+}
+
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+}
+
+async function sendEmail(path, filename, pwd, adressee) {
+    let config = {
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        auth: {
+            user: "antonelgabor2@gmail.com",
+            pass: pwd,
+        },
+    };
+
+    let transporter = mailer.createTransport(config);
+    await transporter.sendMail({
+        from: '"Data loader script" <script_server@gmail.com>',
+        to: adressee,
+        subject: "Some errors occured",
+        text: "Check recap file",
+        attachments: [{
+            filename: filename,
+            path: path
+        }]
+    });
+}
+
+module.exports = {
+    findByKey,
+    subStringCustom,
+    delay,
+    sendEmail
+};
