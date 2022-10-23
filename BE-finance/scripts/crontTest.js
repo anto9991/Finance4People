bff3e1bacc51f738fa2a3e060ecedb98bc50a947
Antonel Gabor <antonelgabor@Antonels-MBP.fritz.box>
2022-10-23 12:08:36 +0200

    Cron test

---
+++ BE-finance/scripts/crontTest.js
@@ -0,0 +1,27 @@
+'use strict'
+const mailer = require('nodemailer');
+const env = require("dotenv").config({
+    path: "../.env",
+}).parsed;
+
+async function sendMail(){
+    let config = {
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        auth: {
+            user: "antonelgabor2@gmail.com",
+            pass: env.GMAIL_PWD,
+        },
+    };
+    
+    let transporter = mailer.createTransport(config);
+    await transporter.sendMail({
+        from: '"167" <script_server@gmail.com>',
+        to: "antonelgabor@gmail.com",
+        subject: "Cron test",
+        text: "Here's today's data load recap"
+    });
+}
+
+sendMail();
