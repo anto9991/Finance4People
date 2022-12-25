8978fa47b51b9a33fdcd7acff9cf3d7f6c4c890f
Antonel Gabor <antonelgabor@Antonels-MBP.station>
2022-12-25 17:48:20 +0100

    Nginx config

--- BE-finance/server.js
+++ BE-finance/server.js
@@ -146,7 +146,7 @@ fastify.register(require("./controller/user"));
 //
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000 })
+    await fastify.listen({ port: 3000, host: '127.0.0.2' })
   } catch (err) {
     // fastify.log.error(err)
     console.log(err)
